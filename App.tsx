1fcadbc5050f3cf120fd374082d715a0c2889677
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2023-05-17 21:37:56 -0300

    commit-fontes

--- App.tsx
+++ App.tsx
@@ -12,11 +12,16 @@ import {
 } from '@expo-google-fonts/bai-jamjuree'
 
 export default function App() {
-  const [] = useFonts({
+  const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold
   })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-gray-900">
       <Text className="text-gray-50 font-bold text-5xl">Rocketseat</Text>

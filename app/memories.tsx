3c5cf29463376375076c59e657e03093bd53ca3d
juliocesarrodriguespiau <juliocrpiau80@gmail.com>
2023-05-22 10:42:16 -0300

    commit-memories

--- app/memories.tsx
+++ app/memories.tsx
@@ -1,9 +1,101 @@
-import { Text, View } from "react-native";
+import { Image, TouchableOpacity, View, Text, ScrollView } from "react-native";
+import Icon from '@expo/vector-icons/Feather'
 
-export default function Memories() {
+import NLWLogo from '../src/assets/nlw-sapcetime-logo.svg'
+import { Link, useRouter } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import * as SecureStore from 'expo-secure-store';
+import { api } from "../src/lib/api";
+import { useEffect, useState } from "react";
+import dayjs from 'dayjs';
+
+interface Memory {
+    coverUrl: string
+    excerpt: string
+    createdAt: string
+    id: string
+}
+
+export default function NewMemory() {
+    const { bottom, top } = useSafeAreaInsets()
+    const router = useRouter()
+    const [memories, setMemories] = useState<Memory[]>([])
+
+    async function signOut() {
+        await SecureStore.deleteItemAsync('token')
+
+        router.push('/')
+    }
+
+    async function loadMemories() {
+        const token = await SecureStore.getItemAsync('token')
+
+        const response = await api.get('/memories', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        setMemories(response.data)
+    }
+
+    useEffect(() => {
+        loadMemories()
+    }, [])
+
     return (
-        <View className="flex-1 justify-center items-center">
-            <Text>Memories</Text>
-        </View>
+        <ScrollView 
+            className="flex-1 px-5" 
+            contentContainerStyle={{ paddingBottom: bottom, paddingTop: top}}
+        >
+            <View className="flex-row mt-4 px-8items-center justify-between">
+                <NLWLogo />
+
+                <View className="flex-row gap-2">
+                    <TouchableOpacity onPress={signOut} className=" h-10 w-10 items-center justify-center rounded-full bg-red-500">
+                        <Icon name="log-out" size={16} color="#000"/>
+                    </TouchableOpacity> 
+
+                    <Link href="/new" asChild>
+                        <TouchableOpacity className=" h-10 w-10 items-center justify-center rounded-full bg-green-500">
+                            <Icon name="plus" size={16} color="#000"/>
+                        </TouchableOpacity>   
+                    </Link> 
+                </View>
+            </View>
+
+            <View className="mt-6 space-y-18">
+                {memories.map(memory => {
+                    return (
+                        <View key={memory.id} className="space-y-4">
+                    <View className="flex-row items-center gap-2">
+                        <View className="h-px w-5 bg-gray-50" />
+                        <Text className="font-body text-xs text-gray-100">
+                            21 de Maio 2023
+                        </Text>
+                    </View>
+                    <View className="space-y-4 px-8">
+                        <Image
+                            source={{ uri: memory.coverUrl,}}
+                            className="aspect-video W-full rounded-lg"
+                            alt=""
+                        />
+                        <Text className="font-body text-base leading-relaxed text-gray-100">
+                            {memory.excerpt}
+                        </Text>
+                        <Link href="/memories/id" asChild>
+                            <TouchableOpacity className="flex-row items-center gap-2">
+                                <Text className="font-body text-sm text-gray-200">
+                                    Ler mais
+                                </Text>
+                                <Icon name="arrow-right" size={16} color="#9e9ea0" />
+                            </TouchableOpacity>
+                        </Link>
+                    </View>
+                </View>
+                    )
+                })}
+            </View>
+        </ScrollView>
     )
 }
--- app/new.tsx
+++ app/new.tsx
@@ -1,15 +1,80 @@
-import { Text, Switch, TouchableOpacity, View, TextInput, ScrollView } from "react-native";
+import { Image, Text, Switch, TouchableOpacity, View, TextInput, ScrollView } from "react-native";
 import Icon from '@expo/vector-icons/Feather'
 
 import NLWLogo from '../src/assets/nlw-sapcetime-logo.svg'
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useState } from "react";
+import * as ImagePicker from 'expo-image-picker';
+import * as SecureStore from 'expo-secure-store';
+import { api } from "../src/lib/api";
 
 export default function NewMemory() {
     const { bottom, top } = useSafeAreaInsets()
+    const router = useRouter()
 
+    const [preview, setPreview] = useState<string | null>(null)
+    const [content, setContent] = useState("")
     const [isPbulic, setIsPublic] = useState(false)
+
+    async function openImagePicker() {
+        try {
+            const result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                quality: 1,
+              })
+
+              console.log(result)
+
+              if (result.assets[0]){
+                setPreview(result.assets[0].uri)
+              }
+        } catch (err) {
+            // deu erro mas eu não tratei
+          }     
+    }
+
+    async function handleCreateMemory() {
+        const token = await SecureStore.getItemAsync('token')
+
+        let coverUrl = ''
+
+        if (preview) {
+            const uploadFormData = new FormData()
+
+            uploadFormData.append('file', {
+                uri: preview,
+                name: 'image.jpg',
+                type: 'image.jpg',
+            }as any)
+
+            const uploadResponse = await api.post('/upload', uploadFormData, {
+                headers: {
+                    'Content-type': 'multipart/form-data'
+                }
+            })
+
+            coverUrl = uploadResponse.data.fileUrl
+
+            console.log(coverUrl)
+        }
+
+        await api.post(
+            '/memories',
+            {
+                content,
+                isPbulic,
+                coverUrl
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            },
+        )
+
+        router.push('/memories')
+    }
 
     return (
         <ScrollView className="flex-1 px-8" contentContainerStyle={{ paddingBottom: bottom, paddingTop: top}}>
@@ -38,25 +103,37 @@ export default function NewMemory() {
 
                 <TouchableOpacity 
                     activeOpacity={0.7}
+                    onPress={openImagePicker}
                     className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
                 >
-                    <View>
-                        <Icon name="image" color="#FFF" />
-                        <Text className="font-body text-sm text-gray-200">
-                            Adicionar foto ou vídeo de capa
-                        </Text>
-                    </View>
+          {preview ? (
+            <Image
+              source={{ uri: preview }}
+              className="h-full w-full rounded-lg object-cover"
+            />
+          ) : (
+            <View className="flex-row items-center gap-2">
+              <Icon name="image" color="#fff" />
+              <Text className="text-sm font-bold text-gray-200">
+                Adicionar foto ou vídeo de capa
+              </Text>
+            </View>
+          )}
+
                 </TouchableOpacity>
 
                 <TextInput
                     multiline
+                    value={content}
+                    onChangeText={setContent}
                     className="p-8 font-body text-lg text-gray-50"
                     placeholderTextColor="#56565a"
                     placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
                 />
 
                 <TouchableOpacity 
-                    activeOpacity={0.7} 
+                    activeOpacity={0.7}
+                    onPress={handleCreateMemory} 
                     className='self-end items-center rounded-full bg-green-500 px-5 py-2'
                 >
                     <Text className='font-alt text-sm uppercase text-black'>
